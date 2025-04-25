730b7d6c048f1b197fa092e47e8555533cb36172
Clemzyrex Jnr <etekaclement77@gmail.com>
2025-04-26 00:54:25 +0100

    add dummy footer to my UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from "./header/navbar/navbar.component";
 import { HeroSectionComponent } from './header/hero-section/hero-section.component';
 import { TestimonialComponent } from "./testimonials/testimonial/testimonial.component";
 import { CourseComponent } from './courses/course/course.component';
+import { FooterComponent } from './footer/footer.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroSectionComponent, TestimonialComponent,CourseComponent],
+  imports: [NavbarComponent, HeroSectionComponent, TestimonialComponent,CourseComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
