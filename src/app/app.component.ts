6f6540707ce063bf0d1a300ea25988fa0d320b06
Clemzyrex Jnr <etekaclement77@gmail.com>
2025-04-26 00:12:01 +0100

    add courses section in my UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "./header/navbar/navbar.component";
 import { HeroSectionComponent } from './header/hero-section/hero-section.component';
 import { TestimonialComponent } from "./testimonials/testimonial/testimonial.component";
+import { CourseComponent } from './courses/course/course.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroSectionComponent, TestimonialComponent],
+  imports: [NavbarComponent, HeroSectionComponent, TestimonialComponent,CourseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
