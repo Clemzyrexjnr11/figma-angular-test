fb1b95525c85ea6fe91bd51ebe400e09e18e3b81
Clemzyrex Jnr <etekaclement77@gmail.com>
2025-04-25 22:56:26 +0100

    add testimonial section tho it needs some adjustment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./header/navbar/navbar.component";
 import { HeroSectionComponent } from './header/hero-section/hero-section.component';
+import { TestimonialComponent } from "./testimonials/testimonial/testimonial.component";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent,HeroSectionComponent],
+  imports: [NavbarComponent, HeroSectionComponent, TestimonialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
